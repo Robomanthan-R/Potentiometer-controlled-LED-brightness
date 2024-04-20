3c02ce7aa6c7eef0bff3ca4f20fce891a392a75d
Robomanthan-R <166908900+Robomanthan-R@users.noreply.github.com>
2024-04-20 22:02:49 +0530

    Add files via upload

---
+++ ledbrightnesspotentiometer.ino
@@ -0,0 +1,11 @@
+const int ledPin = 9; 
+const int potPin = A0; 
+void setup() {
+  pinMode(ledPin, OUTPUT);
+}
+void loop() {
+  int potValue = analogRead(potPin);
+  int brightness = map(potValue, 0, 1023, 0, 255);
+  analogWrite(ledPin, brightness);
+  delay(10);
+}
